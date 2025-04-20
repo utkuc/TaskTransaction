a3e8f47c90fb16551fc5f5226dd0496566fe9c50
Utku Cuhadaroglu <utku.cuhadaroglu@gmail.com>
2025-04-20 12:24:54 +0300

    Create UserService.cs

---
+++ TaskTransaction/Services/UserService.cs
@@ -0,0 +1,26 @@
+﻿using TaskTransaction.Models.User;
+
+namespace TaskTransaction.Services;
+
+public class UserService (ILogger<UserService> logger,IUserRepository userRepository)
+{
+    public async Task<User> GetUserByIdAsync(string userId)
+    {
+        return await userRepository.GetByIdAsync(userId);
+    }
+    public async Task<IEnumerable<User>> GetAllUsersAsync()
+    {
+        return await userRepository.GetAllAsync();
+    }
+
+    public async Task<User> UpdateAsync(User user)
+    {
+        await userRepository.UpdateAsync(user);
+        return user;
+    }
+
+    public async Task<bool> DeleteUserAsync(string userId)
+    {
+        return await userRepository.DeleteAsync(userId);
+    }
+}
